ec466e71e8c5e8fd8614e1c7ea83afc3b15a034a
Agent Ebimene <ebimeneagent@gmail.com>
2022-12-01 20:20:27 +0100

    Create blog application using create-reacyt-app

---
+++ src/components/topBar/TopBar.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./topBar.css";
+import personIcon from "../../assets/images/person-icon.jpg";
+
+const TopBar = () => {
+  return (
+    <div className="top">
+      <div className="topLeft">
+        <i class="fa-brands fa-square-facebook topIcon"></i>
+        <i class="fa-brands fa-twitter topIcon"></i>
+        <i class="fa-brands fa-pinterest topIcon"></i>
+        <i class="fa-brands fa-instagram topIcon"></i>
+      </div>
+      <div className="topCenter">
+        <ul className="topList">
+          <li className="topListItem">HOME</li>
+          <li className="topListItem">ABOUT</li>
+          <li className="topListItem">CONTACT</li>
+          <li className="topListItem">LOGOUT</li>
+        </ul>
+      </div>
+      <div className="topRight">
+        <img src={personIcon} alt="user icon" className="topImg"></img>
+        <i class="topSearchIcon fa-solid fa-magnifying-glass"></i>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
